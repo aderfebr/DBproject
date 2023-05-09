63874c0bff041422f76a0508595dc673044dee6b
aderfebr <415954244@qq.com>
2023-05-09 23:32:34 +0800

    settings更新

--- DBproject/settings.py
+++ DBproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'DBproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crowdvis',
+        'NAME': 'DBproject',
         'HOST': '127.0.0.1',
         'PORT': 3306,
         'USER': 'root',
